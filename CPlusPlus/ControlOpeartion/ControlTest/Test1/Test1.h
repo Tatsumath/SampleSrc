55efeabaf60a81686e1ec1cd0fd43e5a37e2f2aa
Tatsumath <87064492+Tatsumath@users.noreply.github.com>
2025-01-19 08:24:57 +0900

    Control Arrange

---
+++ CPlusPlus/ControlOpeartion/ControlTest/Test1/Test1.h
@@ -0,0 +1,32 @@
+﻿
+// Test1.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CTest1App:
+// このクラスの実装については、Test1.cpp を参照してください
+//
+
+class CTest1App : public CWinApp
+{
+public:
+	CTest1App();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTest1App theApp;
---
+++ CPlusPlus/ControlOpeartion/ControlTest/Test1/Test1Dlg.h
@@ -0,0 +1,34 @@
+﻿
+// Test1Dlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CTest1Dlg ダイアログ
+class CTest1Dlg : public CDialogEx
+{
+// コンストラクション
+public:
+	CTest1Dlg(CWnd* pParent = nullptr);	// 標準コンストラクター
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TEST1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
