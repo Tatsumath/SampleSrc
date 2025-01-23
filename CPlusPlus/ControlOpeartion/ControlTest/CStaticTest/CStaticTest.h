17af3db623227386c3db5b433cfe05a5b4b7e6df
Tatsumath <87064492+Tatsumath@users.noreply.github.com>
2025-01-23 09:19:35 +0900

    Add CStatic Project

---
+++ CPlusPlus/ControlOpeartion/ControlTest/CStaticTest/CStaticTest.h
@@ -0,0 +1,32 @@
+﻿
+// CStaticTest.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CCStaticTestApp:
+// このクラスの実装については、CStaticTest.cpp を参照してください
+//
+
+class CCStaticTestApp : public CWinApp
+{
+public:
+	CCStaticTestApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCStaticTestApp theApp;
---
+++ CPlusPlus/ControlOpeartion/ControlTest/CStaticTest/CStaticTestDlg.h
@@ -0,0 +1,37 @@
+﻿
+// CStaticTestDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CCStaticTestDlg ダイアログ
+class CCStaticTestDlg : public CDialogEx
+{
+// コンストラクション
+public:
+	CCStaticTestDlg(CWnd* pParent = nullptr);	// 標準コンストラクター
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CSTATICTEST_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+private:
+	CStatic m_ctrlStatic;
+};
